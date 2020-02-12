ad91cceb83e3461e619cc1c5a6d51b2cf1e6b74e
Julian Lissner <lissner@mechbau.uni-stuttgart.de>
2020-02-12 15:31:59 +0100

    mostly added documentation, also set further ideas for default publishable plots

--- plt_templates.py
+++ plt_templates.py
@@ -1,49 +1,161 @@
 import matplotlib.pyplot as plt
 from palette import *
 
-def linestyles():
-    """
-    User defined linestyles written in a list.
-    Easily accessed with e.g. ls=linestyles()... ax.plot(..., **ls[i] )
-    The user has to know which line has which attributes
-    """
+import matplotlib.font_manager as fm
 
-    #kwargs = { **{'mec':mec, 'markersize':markersize}, **kwargs} #put some defaults in the kwargs 
-    ##hier definiert man dann quasi die default farbe pro linie
-    #styles.append( dict( ls=linestyle, lw=linewidth, color=uniSblue(), **kwargs) )
-    #styles.append( dict( ls=linestyle, lw=linewidth, color=uniSred(), **kwargs) )
-    ## etc...
+def linestyles( x, y, z, lw=4, ls='-', **kwargs):
+    """
+    Pre defined linecolors given in a list, for each entry in the list there is one color
+    The corporate colors are given in this order:
+    blue, lightblue, green, magenate, gray, red, yellow
+
+    can be used by e.g. plt.plot( x,y, **ls[0] )
+    The given input arguments modify the style for every line.
+
+    Parameters. optional:
+    ---------------------
+    lw          float, default = 4
+                specified linewidth of all lines
+    ls          string, default = '-'
+                specified linestyle of all lines
+    **kwargs    dict
+                additional linestyles specified for each line 
+    To see the available options in kwargs and ls, see "help( plt.plot)" for reference
+
+    Returns:
+    --------
+    linestyles:     list
+                    list of specified linestyles of various colors
+    """
 
     styles = []
-    styles.append( { 'linewidth' : 4, 'color' : '#004191', 'linestyle' : '-' } ) # dark blue
-    styles.append( { 'linewidth' : 4, 'color' : '#00beff', 'linestyle' : '-' } ) # light blue
-    styles.append( { 'linewidth' : 4, 'color' : '#8dc63f', 'linestyle' : '-' } ) # green
-    styles.append( { 'linewidth' : 4, 'color' : '#ec008d', 'linestyle' : '-' } ) # magenta
-    styles.append( { 'linewidth' : 4, 'color' : '#323232', 'linestyle' : '-' } ) # gray
-    styles.append( { 'linewidth' : 4, 'color' : '#ee1c25', 'linestyle' : '-' } ) # red
-    styles.append( { 'linewidth' : 4, 'color' : '#ffdd00', 'linestyle' : '-' } ) # yellow
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSblue'),    linestyle=ls, **kwargs ) ) # dark blue
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSlblue'),   linestyle=ls, **kwargs ) ) # light blue
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSgreen'),   linestyle=ls, **kwargs ) ) # green
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSmagenta'), linestyle=ls, **kwargs ) ) # magenta
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSgray'),    linestyle=ls, **kwargs ) ) # gray
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSred'),     linestyle=ls, **kwargs ) ) # red
+    styles.append( dict( linewidth=lw, color=CDColor( 'uniSyellow'),  linestyle=ls, **kwargs ) ) # yellow
     return styles
 
-def default_figure( n_row=1, n_col=1, figsize=(6,5), other_default='gotta think of them', **kwargs):
+
+def rc_default( fontsize=20, grid=True):
+    """
+    Gives some default parameters set for a nice plot layout
+    This function is to be used in the main as "plt.rcParams.update( rc_default() )"
+    
+    Parameters, optional:
+    ---------------------
+    fontsize:       int, default:20
+                    fontsize of all things in the plot
+    grid:           bool, default: True
+                    If a grid should be plotted per default in each plot
+
+    Returns:
+    --------
+    default_params: dict
+                    dictionary of set default parameters 
+    """
+    default_params = dict() 
+    ## Font specification
+    try:
+        uni_font = [ font for font in fm.findSystemFonts() if ('UniversforUniS65Bd-Regular.ttf' in font ) ][0]
+    except:
+        print( 'specified font for Uni Stuttgart not found, continuing with default font' )
+    default_params.update( { 'font.family':uni_font, 'font.size':fontsize} )
+
+    ## Default Grid
+    if grid:
+        default_params.update( { 'axes.grid':True, 'grid.color':'#AAAAAA', 'grid.linestyle':':', 'grid.linewidth':1.2 } )
+
+    ## Spacing between subplots
+#     'figure.subplot.bottom': 0.11,
+#     'figure.subplot.hspace': 0.2,
+#     'figure.subplot.left': 0.125,
+#     'figure.subplot.right': 0.9,
+#     'figure.subplot.top': 0.88,
+#     'figure.subplot.wspace': 0.2,
+    ## MUSS ICH NOCH FEINTUNEN, sodass es immer hübsch ist
+    return default_params
+
+
+
+def subplots( n_row=1, n_col=1, figsize=None, **kwargs):
+    """
+    Shadows the "plt.subplots" function and adds default styles.  See help( plt.subplots) for reference
+    This function yields optimal results for these subplot layouts: 1x1, 1x2 or 1x3
+     If "figsize" is not set, the spacing between the plots is also adjusted, otherwise it takes matplotlib defaults (for 1x1-1x3 plots)
+    For all other parameters, only the figsize and no spacing is set
+    
+    Parameters, optional:
+    ---------------------
+    n_row:      int, default: 1
+                number of rows for the subplot 
+    n_col:      int, default: 1
+                number of columns for the subplot 
+    figsize:    list like of floats, default: None
+                Set the figsize of the subplots
+    **kwargs:   keyworded arguments
+                arguments which will be put into the plt.subplots(**kwargs) call 
+    Returns:
+    --------
+    fig:        plt.figure object
+                figure handle for plot
+    axes:       plt.axes object or np.ndarray of axes object
+                axes object for each subplot 
+    """
+
+    if figsize is None:
+        if n_col == 1 and n_row == 1:
+            figsize = [ 6/2.54, 5/2.54] ###matplotlib kann nur inches, deswegen muss man hier für CM teilen
+        elif n_col == 2 and n_row == 1:
+            figsize = [ 12/2.54, 5/2,54 ]
+            #fig.subplots.adjust( wspace=xx) #TODO adjust the spacing between the plots
+        elif n_col == 3 and n_row == 1:
+            figsize = [ 15/2.54, 5/2,54 ]
+
+        else:
+            figsize = [ 6*n_col/2.54, 5*n_row/2.54 ]
+            if n_row > 3 or n_col > 3:
+                print( '############### WARNING ###############\nfigsize set by default to {}, for more than 3 rows/columns in the plot could be ugly'.format( figsize) )
+
     fig, axes = plt.subplots( n_row, n_col, figsize=figsize, **kwargs)
-    if axes.ndim > 1:
-        for i in range(axes.shape[0]):
-            for j in range(axes.shape[1]):
-                axes[i,j] = default_layout( axes[i,j], **kwargs)
-    else:
-        for i in range(axes.shape[0]):
-            axes[i] = default_layout( axes[i], **kwargs)
     return fig, axes
 
 
-def default_layout( ax, **kwargs):
+def set_titles( axes, *titles):
     """
-    TODO
+    Set the title of multiple axes objects in one function call
+
+    Parameters:
+    -----------
+    axes:       plt.axes object or np.ndarray of axes objects
+                axes handles on which the titles should be added
+    *titles:    strings
+                multiple strings for the titles, should be as many titles as there are axes handles given
+
+    Returns:
+    --------
+    axes:       plt.axes object or np.ndarray of axes objects
+                axes handles with set titles 
     """
-    ax.grid( linewidth=2.5, color='#AAAAAA', linestyle='--' )
-    #maybe set x and y labels for all the plots with kwargs
-    plt.rcParams.update( {'font.size':16} ) #might not work, to be tested
-    return ax
+
+    if isinstance( axes, np.ndarray):
+        axes_shape = axes.shape
+        if np.sum( axes.shape) != len( titles):
+            print( "################### WARNING #####################\nmismatching number of titles and axes objects given, matching the first 'n axes' with first 'n titles' " )
+        axes = axes.flatten()
+        for i in range( np.min( (len( titles), len(axes) )) ):
+            axes[i].set_title( titles[i] )
+        axes = axes.reshape( axes_shape)
+    else:
+        try:
+            axes.set_title( titles)
+        except:
+            print( "################### WARNING #####################\n title for single axes object could not be set, returning axes with no title added" )
+    return axes
+
+
 
 def add_legend( ax, position='top_right', opacity=0.5, **kwargs):
     """
@@ -68,7 +180,7 @@ def add_legend( ax, position='top_right', opacity=0.5, **kwargs):
         defaults['bbox_to_anchor']=relative_position
     style= {**defaults, **kwargs} #overwrites the defaults by the kwargs
     key = ax.legend(**style)
-    key.get_frame().set_linewidth(5 ) 
+    key.get_frame().set_linewidth( 5) 
     if 'linewidth' in kwargs or 'lw' in kwargs:
         try: 
             key.get_frame().set_linewidth( kwargs['linewidth']) 
@@ -80,9 +192,12 @@ def add_legend( ax, position='top_right', opacity=0.5, **kwargs):
     return ax
 
 
-def axis_labels( ax, x, y, size=16):
-    ax.set_xlabel(x, size=size)
-    ax.set_ylabel(y, size=size)
+def axis_labels( ax, xlabel, ylabel, fontsize=16, **kwargs):
+    """
+    Set the x and y label for a given ax object
+    """
+    ax.set_xlabel(xlabel, size=fontsize, **kwargs)
+    ax.set_ylabel(ylabel, size=fontsize, **kwargs)
     return ax
 
 
@@ -100,4 +215,30 @@ def bounding_lines(ax, horizontal=True, minval=0, maxval=1):
     else:
         ax.axvline(minval, color='#AAAAAA', linewidth=3)
         ax.axvline(maxval, color='#AAAAAA', linewidth=3)
+    return ax
+
+
+def set_grid( ax, lw=2.5, ls=':', color='#AAAAAA'):
+    ## DEPECRATED (is given in the default params function )
+    """
+    Set a grid to the given axes object
+    
+    Paremters:
+    ----------
+    ax:     plt.axes object
+            current axes object to add the grid in
+
+    lw:     float, default: 2.5
+            linewidth of the grid
+    ls:     string, defaiult: ':' 
+            linestyle of the grid
+    color:  string, default: '#AAAAAA'
+            color of the grid
+
+    Returns:
+    --------
+    ax:     plt.axes object
+            axes object with added grid
+    """
+    ax.grid( linewidth=2.5, color='#AAAAAA', linestyle=':' )
     return ax
