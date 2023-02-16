ebca49e50c1c344d310d82c21811d19e5c695664
Julian Lissner <lissner@mechbau.uni-stuttgart.de>
2023-02-16 16:15:41 +0100

    minor consolue output improvement

--- learner_functions.py
+++ learner_functions.py
@@ -235,16 +235,15 @@ class RemoteLR(LRSchedules):
             self.phase += 1
         ## if we are slashing it from outside upon plateau
         elif self.n_up <= self.phase <= (self.n_up + self.n_down):
-            print( 'decreasing learning rate')
+            print( 'decreasing learning rate', end='')
             self.phase += 1
             self.reset_optimizer()
             self.learnrate /= self.jump
             try: 
                 self.optimizer.weight_decay = self.optimizer.weight_decay / 10**0.5
-                print( 'decreasing weight decay' )
-            except: #pass
-                print( 'failed to decrease weight decay' )
-
+                print( ' and weight decay' )
+            except: 
+                print( ', failed to decrease weight decay' ) 
             if self.phase == (self.n_up + self.n_down):
                 print( 'now enabling early stopping switch' )
                 self.allow_stopping = True #finally enable stopping of the training
