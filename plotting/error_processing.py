888670a3e41a6cf09e99f1f41ff5d2db6e0986c3
Julian Lissner <lissner@mechbau.uni-stuttgart.de>
2023-08-09 20:14:20 +0200

    fixed bug for axis labels

--- plotting/error_processing.py
+++ plotting/error_processing.py
@@ -8,6 +8,7 @@ import plt_templates as plot
 import general_functions as func
 from general_functions import Cycler
 from palette import UniStuttgart as uniS
+from palette import color_mixer
 
 plt.rcParams.update( plot.rc_defaults() )
 
@@ -16,7 +17,6 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
     Put the R^2 plot into the given axis. Additionally puts in decorating
     lines which indicate the relative errors.
     Modifies the ax object in place, does not have to be overwritten
-    NOTE that this function does not nicely work for values > 100
     Parameters:
     -----------
     ax:             matplotlib.pyplot ax object
@@ -31,6 +31,8 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
                     if given a market inset will be drawn, the parameters of the box_loc
                     are 'bottom, left, width, height' of the box (positions relative
                     to figure, care!
+                    If True is passed, it is assumed that its a single ax figure from
+                    the plt_template and will locate it accordingly
     rsquare_number: bool, default True
                     if a text decorator should give the rsquare score inside the plot
     axset:          kwargs with default arguments, called in the ax.set( **axset) method
@@ -41,6 +43,9 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
     ax:     matplotlib.pyplot ax object
             axes with the added R^2 plot and decorators
     """
+    if box_loc is True:
+        box_loc = (2/3+0.02, 0.18, 0.27, 0.25)
+    ## input preprocessing
     axset['xlabel'] = axset.pop( 'xlabel', 'true value' )
     axset['ylabel'] = axset.pop( 'ylabel', 'predicted value' )
     ## Preprocessing and allocations
@@ -69,7 +74,7 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
             label = f'$\leq${error}\% error' 
         else:
             label = f'$>${error_bounds[-2]}\% error' 
-        ax.scatter( *2*[-100], color=current_color, **base_kwargs, s=20, label=label)
+        ax.scatter( *2*[-1e10], color=current_color, **base_kwargs, s=20, label=label)
         color[ color_idx] = current_color 
     ## zoom in on the middle part
     dx = xmax-xmin
@@ -92,7 +97,7 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
         mark_inset( ax, box, loc1=1, loc2=3, facecolor=box_color, edgecolor='k', lw=1.5)
     #decorations
     if rsquare_number is True:
-        ax.text( 0.48, 0.92, r'$R^2\approx {:1.4f}$'.format( rsquare),  transform=ax.transAxes )
+        ax.text( 0.48, 0.92, r'$R^2\approx {:1.5f}$'.format( rsquare),  transform=ax.transAxes )
     if xmin < 0:
         ax.set_xlim( xmin=1.05*xmin, xmax=1.05*xmax)
         ax.set_ylim( ymin=1.05*xmin, ymax=1.05*xmax)
@@ -100,12 +105,12 @@ def r_squared_plot( ax, solution, prediction, error_bounds=[1,2,5,10], box_loc=N
         ax.set_xlim( xmin=0.95*xmin, xmax=1.05*xmax)
         ax.set_ylim( ymin=0.95*xmin, ymax=1.05*xmax)
     ax.set( **axset)
-    plot.add_legend( ax, 'top left')
+    plot.add_legend( ax, position='top left')
     return ax
 
 
 
-def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='component wise', quantiles=[0.99, 0.95, 0.90], extra_legend='figures/bin_error_legend.pdf'):
+def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='component wise', quantiles=[0.99, 0.95, 0.90], extra_legend='figures/bin_error_legend.pdf', ylabel=None):
     """
     Scatter the data and the data prediction and set them in relation
     with their error plotted on the -y-axis. The errors are shown for
@@ -131,11 +136,14 @@ def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='
                     must be larger than 50 and sorted descendingly. supports at most 5 quantiles
     extra_legend:   str, default 'figures/bin_error_legend.pdf'
                     if an auxiliary legend should be saved outside of the plots
+    ylabel:         list of 3 str, default None
+                    list of the ylabels, defaults to the thermal problem
     """
     ## Error computation and error binning
     error = y-y_pred
     n_samples = x.shape[0]
-    n_quantiles = len( quantiles)
+    n_quantiles = len( quantiles) 
+    ylabels = [r'$\bar\kappa_{11} [-]$', r'$\bar\kappa_{22} [-]$', r'$\sqrt2\bar\kappa_{12} [-]$' ] if ylabel is None else ylabel
     if error_measure == 'component wise':
         n_target = y.shape[1]
         error = np.sqrt( error**2 )
@@ -175,8 +183,9 @@ def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='
 
 
     fig, axes = plot.fixed_plot(1, n_target, x_stretch=1.3) 
-    reference_style = dict(s=15, color=uniS.lightblue, edgecolor='black')
-    prediction_style = dict(s=5, linewidth=0.02, color=uniS.red, edgecolor='black')#slightly smaller than true values
+    reference_style = dict(s=15, color=uniS.lblue, edgecolor='black')
+    #mixred = color_mixer( uniS.day9yellow, uniS.red )
+    prediction_style = dict(s=4, linewidth=0.002, color=uniS.day9yellow, edgecolor=uniS.red) #slightly smaller than true values
     quantile_colors = [uniS.gray, uniS.gray80, uniS.gray60, uniS.gray40, uniS.gray20 ]# uniS.gray60, uniS.gray70, uniS.gray80, uniS.gray90, uniS.gray100 ]
     quantile_label = ( len(quantiles) ) * '{}, ' + '{}-quantiles'
     quantile_label = quantile_label.format( *quantiles[::-1], 1) 
@@ -201,7 +210,6 @@ def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='
     ## plot decoration
     n_top = 4 #bottom ticks are always 2
     n_bot = 1
-    ylabels = [r'$\bar\kappa_{11} [-]$', r'$\bar\kappa_{22} [-]$', r'$\sqrt2\bar\kappa_{12} [-]$' ]
     for i in range( n_target):
         ylim = 1.05* np.abs(y[:,i]).max()
         bar_tick_incr = max_errors[i,:].max()/n_bot
@@ -210,17 +218,19 @@ def binned_errors( x, y, y_pred, n_bins=15, line_threshold=None, error_measure='
             bottom_ticks = np.arange( -max_errors[i,:].max(), 1e-5, bar_tick_incr )
             yticks = np.around( np.hstack( (bottom_ticks, top_ticks) ), 2)
             ymin = max( -1.35*np.abs(bottom_ticks).max(), -0.5*ylim )
+            axes[i].set_yticks( yticks)
+            axes[i].set_yticklabels( np.abs(yticks) )
         else: #extra treatment of k12
             y_incr = 2*ylim/(2*n_top)
             ymin = -ylim-max_errors[i].max()
             bar_ticks = np.arange(-max_errors[i,:].max(), bar_tick_incr, bar_tick_incr ) 
             bar_ticks = bar_ticks + bar_kwargs['bottom']
             yticks = np.around( np.arange( -ylim+y_incr, ylim, y_incr ), 2)
+            yticklabels =  list( np.abs(np.around( bar_ticks-bar_kwargs['bottom'],2)))+ list(yticks)  
             yticks =  list( bar_ticks) + list(yticks)
-            yticklabels =  list( np.abs(np.around( bar_ticks-bar_kwargs['bottom'],2)))+ list(yticks) 
             axes[i].axhline( bar_kwargs['bottom'] , color='black' )
-        axes[i].set_yticks( yticks)
-        axes[i].set_yticklabels( np.abs(yticks) )
+            axes[i].set_yticks( yticks)
+            axes[i].set_yticklabels( yticklabels )
         axes[i].set_ylabel( ylabels[i])
         axes[i].set_xlabel( 'volume fraction [-]')
         if quantiles:
